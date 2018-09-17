d6191a62685dc4d99a13a5e568d2dbbd0c5e90cb
barpaio <brian.arpaio@linuxacademy.com>
2018-09-17 11:29:42 -0400

    Finished Ng Module lesson in getting started section

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [
-    BrowserModule
-  ],
-  bootstrap: [
-    AppComponent
-  ],
   declarations: [
     AppComponent
-  ]
+  ],
+  imports: [
+    CommonModule,
+    BrowserModule
+  ],
+  bootstrap: [AppComponent];
 })
 export class AppModule {}
