317a554655bf2c928dd2b02740e1115763ed1ad9
barpaio <brian.arpaio@linuxacademy.com>
2018-09-18 10:19:53 -0400

    Finished lesson on property binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,7 +5,9 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      {{ title + '!' }}
+      <h1 [innerHTML]="title"></h1>
+      <h1>{{ title }}</h1>
+      <input type="text" [value]="name">
       <div>
         {{ numberOne + numberTwo }}
       </div>
@@ -17,6 +19,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title: string;
+  name: string = 'Todd';
+  logo: string = 'img/logo.svg';
   isHappy: boolean = true;
   numberOne: number = 1;
   numberTwo: number = 2;
