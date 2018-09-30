812adf579ffb0d8413ef8e919a2e675d7c182692
barpaio <brian.arpaio@linuxacademy.com>
2018-09-30 11:25:52 -0400

    Finsihed lesson on Form Container Data (Lesson 36)

--- app/passenger-dashboard/passenger-dashboard.module.ts
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -4,6 +4,7 @@ import { HttpModule } from '@angular/http';
 
 // containers
 import { PassengerDashboardComponent } from './containers/passenger-dashboard/passenger-dashboard.component';
+import { PassengerViewerComponent } from './containers/passenger-viewer/passenger-viewer.component';
 // components
 import { PassengerCountComponent } from './components/passenger-count/passenger-count.component';
 import { PassengerDetailComponent } from './components/passenger-detail/passenger-detail.component';
@@ -13,6 +14,7 @@ import { PassengerDashboardService } from './passenger-dashboard.service';
 @NgModule({
   declarations: [
     PassengerDashboardComponent,
+    PassengerViewerComponent,
     PassengerCountComponent,
     PassengerDetailComponent
   ],
@@ -21,7 +23,7 @@ import { PassengerDashboardService } from './passenger-dashboard.service';
     HttpModule
   ],
   exports: [
-    PassengerDashboardComponent
+    PassengerViewerComponent
   ],
   providers: [
     PassengerDashboardService
