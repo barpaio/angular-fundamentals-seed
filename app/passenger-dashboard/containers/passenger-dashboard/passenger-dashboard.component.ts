b2247235f72d794cfe3cf97f54b5d71eb23ee2b5
barpaio <brian.arpaio@linuxacademy.com>
2018-10-08 16:18:25 -0400

    Finished lesson on Imperative routing

--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { PassengerDashboardService } from '../../passenger-dashboard.service';
 
@@ -19,6 +20,7 @@ import { Passenger } from '../../models/passenger.interface';
       <passenger-detail
         *ngFor="let passenger of passengers;"
         [detail]="passenger"
+        (view)="handleView($event)"
         (edit)="handleEdit($event)"
         (remove)="handleRemove($event)"
         >
@@ -28,7 +30,10 @@ import { Passenger } from '../../models/passenger.interface';
 })
 export class PassengerDashboardComponent implements OnInit {
   passengers: Passenger[];
-  constructor(private passengerService: PassengerDashboardService) {}
+  constructor(
+    private router: Router,
+    private passengerService: PassengerDashboardService
+  ) {}
   ngOnInit() {
     this.passengerService
       .getPassengers()
@@ -54,5 +59,9 @@ export class PassengerDashboardComponent implements OnInit {
           return passenger.id != event.id;
         });
       });
+  }
+
+  handleView(event: Passenger) {
+    this.router.navigate(['/passengers', event.id]);
   }
 }
