a3f70bd45a6896deb1e652b7a62428459e40ad43
barpaio <brian.arpaio@linuxacademy.com>
2018-10-08 14:09:56 -0400

    Finsihed lesson on understanding routerLink

--- app/not-found.component.ts
+++ app/not-found.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'not-found',
   template: `
-    <div>Address not found</div>
+    <div>Address not found - <a routerLink="/">Go Home</a>?</div>
   `
 })
 
