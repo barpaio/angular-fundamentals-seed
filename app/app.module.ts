6f33b468a0e72c20504cc8e33e787390b9aee135
barpaio <brian.arpaio@linuxacademy.com>
2018-10-08 13:43:44 -0400

    Finished lesson on routes and outlet

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
 
+import { HomeComponent } from './home.component';
 import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     // angular modules
     CommonModule,
     BrowserModule,
+    RouterModule.forRoot(routes),
     // custom modules
     PassengerDashboardModule
   ],
